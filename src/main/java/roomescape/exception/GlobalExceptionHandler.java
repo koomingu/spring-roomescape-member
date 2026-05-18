c0951ace63c480178f02779b3a6f4f301537b133
구민서 <minseo12379@gmail.com>
2026-05-18 14:51:21 +0900

    refactor(GlobalExceptionHandler): 부모 클래스를 오버라이딩하여 스프링 표준 예외를 처리하도록 수정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -3,38 +3,27 @@ package roomescape.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     private final View error;
 
     public GlobalExceptionHandler(View error) {
         this.error = error;
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .findFirst()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .orElse("유효하지 않은 요청입니다");
-
-        ErrorResponse response = new ErrorResponse(message);
-
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(response);
     }
 
     @ExceptionHandler(ApiException.class)
@@ -45,17 +34,55 @@ public class GlobalExceptionHandler {
                 .body(response);
     }
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ErrorResponse> handleMissingParams(MissingServletRequestParameterException e) {
-        String errorMessage = String.format("필수 요청 파라미터(%s)가 누락되었습니다.", e.getParameterName());
-        ErrorResponse response = new ErrorResponse(errorMessage);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("유효하지 않은 요청입니다");
+
+        return createErrorResponseEntity(e, headers, status, request, errorMessage);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        ErrorResponse response = new ErrorResponse("요청 본문이 올바르지 않습니다. JSON 형식을 확인해주세요.");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException e,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        String errorMessage = String.format("필수 요청 파라미터(%s)가 누락되었습니다.", e.getParameterName());
+        return createErrorResponseEntity(e, headers, status, request, errorMessage);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException e,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        String errorMessage = "요청 본문이 올바르지 않습니다. JSON 형식을 확인해주세요.";
+        return createErrorResponseEntity(e, headers, status, request, errorMessage);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            org.springframework.web.HttpRequestMethodNotSupportedException e,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        String errorMessage = String.format("지원하지 않는 HTTP 메서드입니다. 허용된 메서드: %s", String.join(", ", e.getSupportedHttpMethods().stream().map(
+                HttpMethod::toString).toArray(String[]::new)));
+        return createErrorResponseEntity(e, headers, status, request, errorMessage);
     }
 
     @ExceptionHandler(Exception.class)
@@ -65,5 +92,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(response);
+    }
+
+    private ResponseEntity<Object> createErrorResponseEntity(
+            Exception exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request,
+            String message
+    ) {
+        ErrorResponse response = new ErrorResponse(message);
+        return super.handleExceptionInternal(exception, response, headers, status, request);
     }
 }
